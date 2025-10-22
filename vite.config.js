7cf072b6207c32c5f48579bae52004f1df152659
liuc051 <2862552063@qq.com>
2025-10-23 02:56:06 +0800

    修复部署路径和路由模式

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/treasure-hunt-game/', // 替换为你的仓库名
   plugins: [vue()] // 启用 Vue 插件，让 Vite 能解析 .vue 文件
 })
